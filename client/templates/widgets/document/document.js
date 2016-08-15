3a5cef14e01c0211a9c003872fffee4ab61099a1
HunterNL <hunternl@users.noreply.github.com>
2016-08-16 00:16:02 +0200

    can now add field to tr class

--- client/templates/widgets/document/document.js
+++ client/templates/widgets/document/document.js
@@ -11,6 +11,17 @@ Template.document.events({
 });
 
 Template.document.helpers({
-
+  classesFor(document,fields) {
+  
+    var classes = [];
+    
+    fields.forEach(function(field) {
+      if(field.addsClass) {
+        classes.push(document[field.dbField]);
+      }
+    });
+    
+    return classes.join(" ");
+  }
 });
 
